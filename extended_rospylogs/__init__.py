86bc26837008e63f17e048db9393942c94609a25
charlielito <candres.alv@gmail.com>
2018-04-05 20:39:59 -0700

    Add node name when logging

--- extended_rospylogs/__init__.py
+++ extended_rospylogs/__init__.py
@@ -5,6 +5,10 @@ def log_cond(cond, msg, logfunction, *args, **kwargs):
     if type(cond) != bool:
         raise TypeError("First arguemnt needs to be boolean -> Received: {}".format(type(cond)))
     if cond:
+        append_name = kwargs.pop('show_name', True)
+        if append_name:
+            name = rospy.get_name().split("/")[-1] #get name and remove "/"
+            msg = "[" + name.upper() + "] " + nmsg
         logfunction(msg, *args, **kwargs)
 
 def logdebug_cond(cond, msg, *args, **kwargs):
